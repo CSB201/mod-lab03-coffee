c39d9c5b09b895a25f64fa0ff5100e7a4995823c
CSB201 <161685472+CSB201@users.noreply.github.com>
2024-04-03 21:58:34 +0300

    Create Automata.h

---
+++ include/Automata.h
@@ -0,0 +1,44 @@
+// Copyright 2024 CSB_lab3
+
+#ifndef AUTOMATA_H
+#define AUTOMATA_H
+
+#include <iostream>
+#include<vector>
+
+enum class STATES {
+
+	OFF,
+	WAIT,
+	ACCEPT,
+	CHECK,
+	COOK,
+};
+
+class Automata
+{
+
+private:
+	int cash = 0;
+	std::vector<std::string> menu;
+	std::vector<int> price;
+	STATES state = STATES::OFF;
+
+public:
+	int google();
+	void setMenu(std::vector<std::string>);
+	void setPrice(std::vector<int>);
+	std::string statusCheck();
+	void on();
+	void off();
+	void coin(int);
+	void getMenu();
+	void getState();
+	void choice(int);
+	bool check(int);
+	void cancel();
+	void cook(int);
+	void finish();
+};
+
+#endif
