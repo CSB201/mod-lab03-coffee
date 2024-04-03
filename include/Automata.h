2ea8424d1085e27572d35b7730e9895839f3748d
CSB201 <161685472+CSB201@users.noreply.github.com>
2024-04-03 23:01:21 +0300

    cpplint

--- include/Automata.h
+++ include/Automata.h
@@ -7,38 +7,33 @@
 #include<vector>
 
 enum class STATES {
-
-	OFF,
-	WAIT,
-	ACCEPT,
-	CHECK,
-	COOK,
+    OFF,
+    WAIT,
+    ACCEPT,
+    CHECK,
+    COOK,
 };
 
-class Automata
-{
-
-private:
-	int cash = 0;
-	std::vector<std::string> menu;
-	std::vector<int> price;
-	STATES state = STATES::OFF;
-
-public:
-	int google();
-	void setMenu(std::vector<std::string>);
-	void setPrice(std::vector<int>);
-	std::string statusCheck();
-	void on();
-	void off();
-	void coin(int);
-	void getMenu();
-	void getState();
-	void choice(int);
-	bool check(int);
-	void cancel();
-	void cook(int);
-	void finish();
+class Automata {
+ private:
+    int cash = 0;
+    std::vector<std::string> menu;
+    std::vector<int> price;
+    STATES state = STATES::OFF;
+ public:
+    int google();
+    void setMenu(std::vector<std::string>);
+    void setPrice(std::vector<int>);
+    std::string statusCheck();
+    void on();
+    void off();
+    void coin(int);
+    void getMenu();
+    void getState();
+    void choice(int);
+    bool check(int);
+    void cancel();
+    void cook(int);
+    void finish();
 };
-
 #endif
