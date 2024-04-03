0e83dc27124a2e7cc5ae30bc22eddd23aadd0301
CSB201 <161685472+CSB201@users.noreply.github.com>
2024-04-03 22:01:49 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,4 +1,82 @@
-// Copyright 2022 GHA Test Team
+// Copyright 2024 CSB_lab3
 
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
 #include "Automata.h"
+
+TEST(test, on) {
+    Automata test;
+    test.on();
+    EXPECT_EQ("ON", test.statusCheck());
+}
+
+TEST(test, incorrectAction) {
+    Automata test;
+    test.coin(10);
+    test.coin(20);
+    EXPECT_EQ("OFF", test.statusCheck());
+}
+
+TEST(test, off) {
+    Automata test;
+    test.on();
+    test.off();
+    EXPECT_EQ("OFF", test.statusCheck());
+}
+
+TEST(test, coin) {
+    Automata test;
+    test.on();
+    test.coin(10);
+    EXPECT_EQ("ACCEPT", test.statusCheck());
+}
+
+TEST(test, nohMoney) {
+    Automata test;
+    test.on();
+    test.coin(5);
+    test.choice(1);
+    EXPECT_EQ(false, test.check(0));
+}
+
+TEST(test, okMoney) {
+    Automata test;
+    test.on();
+    test.coin(100);
+    test.choice(1);
+    EXPECT_EQ(true, test.check(0));
+}
+
+TEST(test, cancel) {
+    Automata test;
+    test.on();
+    test.coin(200);
+    test.cancel();
+    EXPECT_EQ("WAIT", test.statusCheck());
+}
+
+TEST(test, cook) {
+    Automata test;
+    test.on();
+    test.coin(100);
+    test.choice(2);
+    test.cook(1);
+    EXPECT_EQ("COOK", test.statusCheck());
+}
+
+TEST(test, finish) {
+    Automata test;
+    test.on();
+    test.coin(100);
+    test.choice(2);
+    test.cook(1);
+    test.finish();
+    EXPECT_EQ("WAIT", test.statusCheck());
+}
+
+TEST(test, invalidProduct) {
+    Automata test;
+    test.on();
+    test.coin(50);
+    test.choice(8);
+    EXPECT_EQ("ACCEPT", test.statusCheck());
+}
