3eac0dc5c930f17d197073227c015a9450fd4c8a
CSB201 <161685472+CSB201@users.noreply.github.com>
2024-04-03 23:03:16 +0300

    cpplint

--- src/main.cpp
+++ src/main.cpp
@@ -3,19 +3,18 @@
 #include <iostream>
 #include "Automata.h"
 
-int main()
-{
-	Automata Bumblebee;
+int main() {
+    Automata Bumblebee;
 
-	Bumblebee.setMenu({ "Tea", "Espresso", "Latte", "Hot chocolate"});
-	Bumblebee.setPrice({ 5, 10, 15, 8 });
-	Bumblebee.getMenu();
-	Bumblebee.statusCheck();
-	Bumblebee.on();
-	Bumblebee.coin(10);
-	Bumblebee.coin(10);
-	Bumblebee.choice(4);
-	Bumblebee.off();
-	Bumblebee.getState();
-	return 0;
+    Bumblebee.setMenu({ "Tea", "Espresso", "Latte", "Hot chocolate"});
+    Bumblebee.setPrice({ 5, 10, 15, 8 });
+    Bumblebee.getMenu();
+    Bumblebee.statusCheck();
+    Bumblebee.on();
+    Bumblebee.coin(10);
+    Bumblebee.coin(10);
+    Bumblebee.choice(4);
+    Bumblebee.off();
+    Bumblebee.getState();
+    return 0;
 }
